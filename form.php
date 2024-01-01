1dfb6ea87c4d53bbc6dbb1f79377c98f23a13508
Saiful17699 <151745320+Saiful17699@users.noreply.github.com>
2024-01-01 17:40:59 +0600

    form.php

--- form.php
+++ form.php
@@ -5,7 +5,7 @@ $subject = $_POST['subject'];
 $message = $_POST['message'];
 
 
-$email_from = 'saifulislam.com';
+$email_from = 'saifulislam17699.com';
 
 
 $email_subject = 'New Form Submission';
@@ -13,14 +13,14 @@ $email_subject = 'New Form Submission';
 $email_body = "User Name: $name.\n".
                 "User Email: $visitor_email.\n".
                   "Subject: $subject.\n".
-                   "User Message: $message .\n".;
+                   "User Message: $message .\n";
                    
                    
 $to = 'saifulislam17699@gmail.com';
 
 $headers = "From: $email_from \r\n";
 
-$headers .="Reply-To: $visitor_email \r\n";
+$headers ="Reply-To: $visitor_email \r\n";
 
                    
 mail($to,$email_subject,$email_body,$headers);
